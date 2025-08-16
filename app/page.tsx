2d37505f7ffa5574d3c6c3aaabf48390d48555fb
Max Zelarayán <max.z_@hotmail.com>
2025-08-15 22:56:03 -0300

    launch this

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,10 @@
 import { useState } from "react"
 import ImageGrid from "./components/ImageGrid"
 import { shuffleArray } from "./lib/constants"
-import list from "./lib/list.json"
 
 export default function Home() {
-  const { images } = list
-  const PHOTOS_STEP = 20
+  const images = Array.from(Array(26).keys()).map((i) => i + 1)
+  const PHOTOS_STEP = 6
   const frag = shuffleArray([...images]).slice(0, PHOTOS_STEP)
   const [imgList, setImgList] = useState(frag)
   const [lastImage, setLastImage] = useState(PHOTOS_STEP)
@@ -22,19 +21,25 @@ export default function Home() {
 
   return (
     <div className="mt-8 h-full w-full bg-gray-200 md:mt-20">
-      <h3 className="section__subtitle text-4xl">Lalily14_02</h3>
-      <h2 className="section__title text-2xl">Gustavo Pagani</h2>
+      <h3 className="text-4xl text-center text-blue-600">
+        <a href="https://www.instagram.com/lalily14_02">
+          Instagram @Lalily14_02
+        </a>
+      </h3>
+      <h2 className="text-black text-2xl text-center">Gustavo Pagani</h2>
       <div className="flex flex-col">
-        {/* <ImageGrid images={imgList} /> */}
+        <ImageGrid images={imgList} />
       </div>
-      <div className="w-full p-8 text-center">
-        <button
-          className="g-blue-500 rounded bg-indigo-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-          onClick={viewMoreHandler}
-        >
-          Ver mas
-        </button>
-      </div>
+      {imgList.length !== 26 && (
+        <div className="w-full p-8 text-center">
+          <button
+            className="g-blue-500 rounded bg-indigo-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            onClick={viewMoreHandler}
+          >
+            Ver mas
+          </button>
+        </div>
+      )}
     </div>
   )
 }
